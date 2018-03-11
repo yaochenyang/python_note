d66321771a0e92986d8b840a2dc6d46325b87325
yaochenyang <alexmercer@yeah.net>
2018-03-11 16:14:42 +0800

    change

--- function_note/seek_str.py
+++ function_note/seek_str.py
@@ -7,9 +7,9 @@ def findstr(total, target):
         for i in range(length - 1):  #因为是查询两个字符的字符串,如果倒数第二个不是其中之一,最后一个也不必查询了
             if (total[i] == target[0]) and (total[i + 1] == target[1]):
                 count += 1
-    print(count)
+    return count
 
 
 total = input('请输入总字符串')
 target = input('请输入要查询的两位字符串')
-findstr(total, target)
+print(findstr(total, target))
